bf9e6e0d33cdf1da2ed8e494ee74e1cf46296c8b
udeogu ifechuwku <ifebrand6@gmail.com>
2020-01-29 14:43:27 +0100

    Imported React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
      <h1>Cisca Planner</h1>
     </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+ 
