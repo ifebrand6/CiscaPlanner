4477867164909034c40c844451b372ce8bfb9fa3
udeogu ifechuwku <ifebrand6@gmail.com>
2020-01-29 17:18:15 +0100

    added projectSummary to make App more Modular

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
 import React from "react";
 // import { NavLink } from "react-router-dom";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-
-            <div className="card z-depth-o project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="text-grey">#rd September, 2am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-o project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="text-grey">#rd September, 2am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-o project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="text-grey">#rd September, 2am</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { NavLink } from "react-router-dom";
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-o project-summary">
+        <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by the Net Ninja</p>
+            <p className="text-grey">#rd September, 2am</p>
+        </div>
+    </div>
+    )
+}
+export default ProjectSummary;
