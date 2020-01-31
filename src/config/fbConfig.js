39bfd322daa56be0c0f9eaafc3484ffa98c8678d
udeogu ifechuwku <ifebrand6@gmail.com>
2020-01-31 06:29:07 +0100

    firebas setups

---
+++ src/config/fbConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyByOlVqCpIDzNiFsoaMNJ4OLzAQ4l3DY1A",
+    authDomain: "cisca-planner.firebaseapp.com",
+    databaseURL: "https://cisca-planner.firebaseio.com",
+    projectId: "cisca-planner",
+    storageBucket: "cisca-planner.appspot.com",
+    messagingSenderId: "865934268718",
+    appId: "1:865934268718:web:79b8c5756e1f554dddc2bf",
+    measurementId: "G-SJBRDWWNBJ"
+};
