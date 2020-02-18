ff8d02315dad1c275f90661b06bb830888fd0550
udeogu ifechuwku <ifebrand6@gmail.com>
2020-02-18 07:04:54 +0100

    Updates

---
+++ precache-manifest.352ad17e496a4ef32ad356ec0cd69a4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c3fbbfcae949416202e53b366e0981",
+    "url": "/CiscaPlanner/index.html"
+  },
+  {
+    "revision": "9bbe4f1f9fc8d8a39598",
+    "url": "/CiscaPlanner/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c586d17045b9a544f24",
+    "url": "/CiscaPlanner/static/js/2.0984f8d1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/CiscaPlanner/static/js/2.0984f8d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bbe4f1f9fc8d8a39598",
+    "url": "/CiscaPlanner/static/js/main.b9f5d7d3.chunk.js"
+  },
+  {
+    "revision": "a24bc1044d09071ff8ce",
+    "url": "/CiscaPlanner/static/js/runtime-main.54ac5828.js"
+  }
+]);
